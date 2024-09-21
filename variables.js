e0ec287bd9430ce0f3e8e25430c456d1ecb4a1f4
Mansoor Ahmed <ahmed.mansoor@siemens.com>
2024-09-21 12:50:03 +0530

    updated comments

--- variables.js
+++ variables.js
@@ -1,39 +1,38 @@
+{
+  var a = "10";
+  console.log(a);
+  var a = "11";
+  console.log(a);
 
-  {
-    var a = "10";
-    console.log(a);
-    var a = "11";
-    console.log(a);
-
-    /*
-     1. variable var can be redeclared and reassigned multiple within same block or other block
-    
+  /*
+    var keyword is function scope/global scoped
+    var keyword can re declared and re assigned within function
     */
 
-    let b = "12";
-    console.log(b);
-    //let b;
-    
-    /*
-    1. variable let cannot be redeclared more than once within same block.
-    2. variable let can be redeclared more than once outside different block of code.
-    3. variable let cannot be re assigned more than once within same block.
-    4. variable let can be re assigned more than once outside different block of code.// skeptical
-    */
-    
-    const c = "13";
-    console.log(c);
-    //const c = "15";
-    console.log(c);
-
-    /*
-    1. variable const cannot be re declared and re assigned any where within same block or other block 
-    */
-
-
-    console.log(typeof(c));// typeof() method is used to determine datatype of any variable
-  }
-
-  let b = "14"+b;
+  let b = "12";
   console.log(b);
+  //let b;
 
+  /*
+    let keyword is block scoped
+    let keyword cannot declared and can be re assigned within block
+    */
+
+  const c = "13";
+  console.log(c);
+  //const c = "15";
+  console.log(c);
+
+  /*
+   const keyword is block scoped
+   const keyword cannot be redeclared and cannot be re assigned within block
+   
+   */
+
+  console.log(typeof c); // typeof() method is used to determine datatype of any variable
+}
+
+const c = 19;
+console.log(c);
+// b = "14" + b;
+console.log(b);
