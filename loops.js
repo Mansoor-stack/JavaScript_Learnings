9aeab88fd05d85c5282bbb3ef9a72e2a98fcd7f2
Mansoor Ahmed <ahmed.mansoor@siemens.com>
2024-09-22 09:43:15 +0530

    Adding loops code

---
+++ loops.js
@@ -0,0 +1,40 @@
+// let x=13;
+// if(x>10){
+// console.log("The entered number "+x+ " is greater than 10");
+
+// }
+// else{
+//     console.log("The entered number "+x+ " is not greater than 10")
+// }
+
+// let Y = "Ahmed";
+// if(Y=="Mansoor"){
+// console.log("condition satisfied");
+// }else if(Y=="Ahmed"){
+//     console.log("condition satisfied");
+// }else if(Y=="Farheen"){
+//     console.log("condition satisfied");
+
+// }else
+// console.log("All above condition not satisfied");
+
+let a = 13;
+switch (a) {
+  case 12:
+    console.log("The entered number " + a + " is greater than 12");
+    break;
+  case 13:
+    console.log("The entered number " + a + " is greater than 12");
+    break;
+
+    default:
+        console.log("default block");
+}
+
+let b = 12;
+let c = 13;
+let max;
+
+max = (b>c)?"True":"False";
+
+console.log(max);
