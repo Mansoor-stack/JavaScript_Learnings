e2ff5b6f9502d3c831f8424f1d1eb34c7a989bd7
Mansoor Ahmed <ahmed.mansoor@siemens.com>
2024-10-06 21:57:17 +0530

    r

---
+++ Array.js
@@ -0,0 +1,38 @@
+{
+  let array1 = Array(6);
+  array1 = new Array(1, 2, 3, 4, 5);
+  //console.log(array1.length);
+  //console.log(array1[1]);
+
+  array1.push(6); // add elements at end of an array
+  //console.log(array1)
+  array1.pop(); // removes elements from end of an array
+  //console.log(array1)
+
+  array1.shift(); //remove elements from starting of an array
+  //console.log(array1);
+
+  array1.unshift(1); // add elements at starting of an array
+  console.log(array1);
+
+  let sliced = array1.slice(1, 4); // slice(start index, end index) creates a another array from start index to end index inclusive
+
+  console.log(sliced);
+
+  let spliced = array1.splice(4, 1, 6); //spliced(start index, no of items to remove, replacing_item)
+
+  console.log(spliced); //returns removed element
+  console.log(array1);
+
+  let filtered_array = array1.filter((a) => {
+    return a % 2 === 0;
+  }); // filter() method - learn from code with harry blog and programiz
+
+  console.log(filtered_array);
+
+  let reduced_array = array1.reduce((a, b) => {
+    return a + b;
+  }); // reduce() method - learn from code with harry blog and programiz
+
+  console.log(reduced_array);
+}
